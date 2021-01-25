592646ccf063014925648bced64ab1a5ec9e8b87
Richard Meissner <msc.meissner@gmail.com>
2021-01-25 03:10:43 +0100

    Add test for critical negative flows

--- contracts/test/TestExecutor.sol
+++ contracts/test/TestExecutor.sol
@@ -8,10 +8,15 @@ contract TestExecutor {
         module = _module;
     }
 
-    function call(address payable to, uint256 value, bytes calldata data) external {
+    function exec(address payable to, uint256 value, bytes calldata data) external {
         bool success;
-        (success,) = to.call{value: value}(data);
-        require(success, "Call failed");
+        bytes memory response;
+        (success,response) = to.call{value: value}(data);
+        if(!success) {
+            assembly {
+                revert(add(response, 0x20), mload(response))
+            }
+        }
     }
 
     function execTransactionFromModule(address payable to, uint256 value, bytes calldata data, uint8 operation)
--- test/DelayedTxModule.spec.ts
+++ test/DelayedTxModule.spec.ts
@@ -2,7 +2,6 @@ import { expect } from "chai";
 import { Contract, BigNumber } from "ethers";
 import hre, { deployments, ethers, waffle } from "hardhat";
 import "@nomiclabs/hardhat-ethers";
-import { Deployment } from "hardhat-deploy/types";
 
 describe("DelayedTxModule", async () => {
 
@@ -38,7 +37,7 @@ describe("DelayedTxModule", async () => {
             const { executor } = await setupTest();
             const module = await getModule();
             const updateConfigData = module.interface.encodeFunctionData("updateConfig", [announcer.address, 1, false]);
-            await executor.call(module.address, 0, updateConfigData);
+            await executor.exec(module.address, 0, updateConfigData);
             await expect(
                 await module.configs(executor.address, announcer.address)
             ).to.be.deep.equal([BigNumber.from(1), false]);
@@ -56,7 +55,7 @@ describe("DelayedTxModule", async () => {
             const { executor } = await setupTest();
             const module = await getModule();
             const updateConfigData = module.interface.encodeFunctionData("updateConfig", [announcer.address, 1, false]);
-            await executor.call(module.address, 0, updateConfigData);
+            await executor.exec(module.address, 0, updateConfigData);
             await expect(
                 await module.configs(executor.address, announcer.address)
             ).to.be.deep.equal([BigNumber.from(1), false]);
@@ -81,7 +80,7 @@ describe("DelayedTxModule", async () => {
             const { executor } = await setupTest();
             const module = await getModule();
             const updateConfigData = module.interface.encodeFunctionData("updateConfig", [announcer.address, 1, true]);
-            await executor.call(module.address, 0, updateConfigData);
+            await executor.exec(module.address, 0, updateConfigData);
             await expect(
                 await module.configs(executor.address, announcer.address)
             ).to.be.deep.equal([BigNumber.from(1), true]);
@@ -106,7 +105,7 @@ describe("DelayedTxModule", async () => {
             const { executor } = await setupTest();
             const module = await getModule();
             const updateConfigData = module.interface.encodeFunctionData("updateConfig", [announcer.address, 1, true]);
-            await executor.call(module.address, 0, updateConfigData);
+            await executor.exec(module.address, 0, updateConfigData);
             await expect(
                 await module.configs(executor.address, announcer.address)
             ).to.be.deep.equal([BigNumber.from(1), true]);
@@ -120,11 +119,169 @@ describe("DelayedTxModule", async () => {
             await expect(
                 module.announceTransaction(executor.address, to, value, data, operation, nonce)
             ).to.emit(module, 'NewAnnouncement').withArgs(executor.address, announcer.address, txHash)
-            await module.announcements(txHash)
 
             await expect(
                 module.announceTransaction(executor.address, to, value, data, operation, nonce)
             ).to.revertedWith("Could not announce transaction")
+        })
+    })
+
+    describe("execute tx", async () => {
+        it("throws if not announced", async () => {
+            const { executor } = await setupTest();
+            const module = await getModule();
+            await executor.setModule(module.address);
+            const to = user1.address;
+            const value = 0;
+            const data = "0x";
+            const operation = 0;
+            const nonce = 0;
+            await expect(
+                module.executeTransaction(executor.address, to, value, data, operation, nonce)
+            ).to.be.revertedWith("Could not find announcement");
+        })
+        
+
+        it("throws if executed twice", async () => {
+            const { executor } = await setupTest();
+            const module = await getModule();
+            const updateConfigData = module.interface.encodeFunctionData("updateConfig", [announcer.address, 1, true]);
+            await executor.exec(module.address, 0, updateConfigData);
+            await expect(
+                await module.configs(executor.address, announcer.address)
+            ).to.be.deep.equal([BigNumber.from(1), true]);
+            await executor.setModule(module.address);
+            const to = user1.address;
+            const value = 0;
+            const data = "0x";
+            const operation = 0;
+            const nonce = 0;
+            const txHash = await module.getTransactionHash(executor.address, to, value, data, operation, nonce)
+            await expect(
+                module.announceTransaction(executor.address, to, value, data, operation, nonce)
+            ).to.emit(module, 'NewAnnouncement').withArgs(executor.address, announcer.address, txHash)
+            await module.executeTransaction(executor.address, to, value, data, operation, nonce)
+
+            await expect(
+                module.executeTransaction(executor.address, to, value, data, operation, nonce)
+            ).to.revertedWith("Cannot execute transaction again")
+        })
+        
+        it("throws if not possible to execute yet", async () => {
+            const { executor } = await setupTest();
+            const module = await getModule();
+            const updateConfigData = module.interface.encodeFunctionData("updateConfig", [announcer.address, 4242424242, true]);
+            await executor.exec(module.address, 0, updateConfigData);
+            await expect(
+                await module.configs(executor.address, announcer.address)
+            ).to.be.deep.equal([BigNumber.from(4242424242), true]);
+            await executor.setModule(module.address);
+            const to = user1.address;
+            const value = 0;
+            const data = "0x";
+            const operation = 0;
+            const nonce = 0;
+            const txHash = await module.getTransactionHash(executor.address, to, value, data, operation, nonce)
+            await expect(
+                module.announceTransaction(executor.address, to, value, data, operation, nonce)
+            ).to.emit(module, 'NewAnnouncement').withArgs(executor.address, announcer.address, txHash)
+            await expect(
+                module.executeTransaction(executor.address, to, value, data, operation, nonce)
+            ).to.revertedWith("Cannot execute transaction yet")
+        })
+        
+        it("throws if required announcer not available anymore", async () => {
+            const { executor } = await setupTest();
+            const module = await getModule();
+            const updateConfigData = module.interface.encodeFunctionData("updateConfig", [announcer.address, 1, true]);
+            await executor.exec(module.address, 0, updateConfigData);
+            await expect(
+                await module.configs(executor.address, announcer.address)
+            ).to.be.deep.equal([BigNumber.from(1), true]);
+            await executor.setModule(module.address);
+            const to = user1.address;
+            const value = 0;
+            const data = "0x";
+            const operation = 0;
+            const nonce = 0;
+            const txHash = await module.getTransactionHash(executor.address, to, value, data, operation, nonce)
+            await expect(
+                module.announceTransaction(executor.address, to, value, data, operation, nonce)
+            ).to.emit(module, 'NewAnnouncement').withArgs(executor.address, announcer.address, txHash)
+            
+            const resetConfigData = module.interface.encodeFunctionData("updateConfig", [announcer.address, 0, false]);
+            await executor.exec(module.address, 0, resetConfigData);
+            await expect(
+                await module.configs(executor.address, announcer.address)
+            ).to.be.deep.equal([BigNumber.from(0), false]);
+
+            await expect(
+                module.executeTransaction(executor.address, to, value, data, operation, nonce)
+            ).to.revertedWith("Could not find valid config for executor and announcer")
+        })
+        
+        it("does not throws if announcer not available anymore, but not required", async () => {
+            const { executor } = await setupTest();
+            const module = await getModule();
+            const updateConfigData = module.interface.encodeFunctionData("updateConfig", [announcer.address, 1, false]);
+            await executor.exec(module.address, 0, updateConfigData);
+            await expect(
+                await module.configs(executor.address, announcer.address)
+            ).to.be.deep.equal([BigNumber.from(1), false]);
+            await executor.setModule(module.address);
+            const to = user1.address;
+            const value = 0;
+            const data = "0x";
+            const operation = 0;
+            const nonce = 0;
+            const txHash = await module.getTransactionHash(executor.address, to, value, data, operation, nonce)
+            await expect(
+                module.announceTransaction(executor.address, to, value, data, operation, nonce)
+            ).to.emit(module, 'NewAnnouncement').withArgs(executor.address, announcer.address, txHash)
+            
+            const resetConfigData = module.interface.encodeFunctionData("updateConfig", [announcer.address, 0, false]);
+            await executor.exec(module.address, 0, resetConfigData);
+            await expect(
+                await module.configs(executor.address, announcer.address)
+            ).to.be.deep.equal([BigNumber.from(0), false]);
+
+            await module.executeTransaction(executor.address, to, value, data, operation, nonce)
+        })
+    })
+
+    describe("revoke announcement", async () => {
+        it("throws if not announced", async () => {
+            await setupTest();
+            const module = await getModule();
+            const to = user1.address;
+            const value = 0;
+            const data = "0x";
+            const operation = 0;
+            const nonce = 0;
+
+            await expect(
+                module.revokeTransactionAnnouncement(to, value, data, operation, nonce)
+            ).to.be.revertedWith("Could not find announcement");
+        })
+
+        it("throws if already executed", async () => {
+            const { executor } = await setupTest();
+            const module = await getModule();
+            const updateConfigData = module.interface.encodeFunctionData("updateConfig", [announcer.address, 1, true]);
+            await executor.exec(module.address, 0, updateConfigData);
+            await executor.setModule(module.address);
+            const to = user1.address;
+            const value = 0;
+            const data = "0x";
+            const operation = 0;
+            const nonce = 0;
+            await module.announceTransaction(executor.address, to, value, data, operation, nonce)
+            await module.executeTransaction(executor.address, to, value, data, operation, nonce)
+
+            const revokeAnnouncement = module.interface.encodeFunctionData("revokeTransactionAnnouncement", [to, value, data, operation, nonce]);
+            await expect(
+                executor.exec(module.address, 0, revokeAnnouncement)
+            ).to.be.revertedWith("Cannot revoke executed transaction");
         })
     })
 })
